ae81e03d1fd63a25dea4c0f2d59e1bf3c61d8d25
AnuRag-18 <madhavarapurao720@gmail.com>
2019-01-24 13:09:42 +0530

    push and pop

--- singlyLinkedList_push.js
+++ singlyLinkedList_push.js
@@ -24,8 +24,28 @@ class LinkedList{
         this.length++;
         return this;
    }
+   pop(){
+       if(!this.head) return undefined;
+       var current = this.head;
+       var newTail= current;
+       while(current.next){
+            newTail= current;
+            current=current.next;
+          }
+          this.tail = newTail;
+          this.tail.next==null;
+          this.length--;
+          return current;
+          if(this.length===0){
+              this.head=null;
+              this.tail=null;
+          }
+          
+        }
 }
 var list = new LinkedList();
 list.push("hello");
 list.push('hi AnuRag');
+list.push("rao");
+list.pop('rao');
 console.log(list);
