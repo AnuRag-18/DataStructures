e9bdec01d2b4c58be5a9ff62b66ab62055e50b96
AnuRag-18 <madhavarapurao720@gmail.com>
2019-01-26 16:32:35 +0530

    binary max tree insert and extract max

--- Tree.js
+++ Tree.js
@@ -76,13 +76,72 @@ class BST{
         }
         return false;
     }
+    BFS(){
+        var data = [];
+        var queue =[];
+        var node = this.root;
+        queue.push(this.root);
+        while(queue.length){
+            node = queue.shift();
+            data.push(node.val);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        console.log("bcjbijqi",data);
+        return data;   
+       
+    }
+    DFSpreorder(){
+        var data = [];
+        function helper(node){
+            data.push(node.val);
+            if(node.left) helper(node.left);
+            if(node.right) helper(node.right);
+        }
+        helper(this.root);
+        console.log("preorder",data);
+        return data;
+    }
+    DFSinorder(){
+        var data = [];
+       function helper(node){
+            if(node.left)  helper(node.left);
+            data.push(node.val);
+            if(node.right) helper(node.right);
+        }
+        helper(this.root);
+        console.log("inorder",data);
+        return data;
+    }
+    DFSpostorder(){
+        var data = [];
+       function helper(node){
+            if(node.left)  helper(node.left);
+            if(node.right) helper(node.right);
+            data.push(node.val);
+        }
+        helper(this.root);
+        console.log("postorder",data);
+        return data;
+    }
+   
 }
 var tree = new BST();
 tree.insert(10);
 tree.insert(2);
 tree.insert(15);
-tree.insert(25);
-tree.find(2);
+tree.insert(45);
+tree.insert(30);
+tree.insert(5);
+tree.insert(89);
+tree.insert(8);
+
+tree.BFS();
+tree.DFSinorder();
+tree.DFSpostorder();
+tree.DFSpreorder();
+// tree.insert(25);
+// tree.find(2);
 // tree.root= new Node(10);
 // tree.root.right=new Node(15);
 // tree.root.left=new Node(9);
