052780513ab22cf4c68a2591491dd1b19ab93491
AnuRag-18 <madhavarapurao720@gmail.com>
2019-01-28 18:33:27 +0530

    BFS,DFS

--- Graph.js
+++ Graph.js
@@ -27,18 +27,83 @@ class Graph{
         }
         delete this.ajacencyList[vertex];
     }
+    depthFirstSearchRecursive(start){
+        var result = [];
+        var visited = {};
+        var ajacencyList = this.ajacencyList;
+        (function dfs(vertex){
+           if(!vertex) return null;
+           visited[vertex]=true ;
+           result.push(vertex);
+           ajacencyList[vertex].forEach(neighbour =>{
+               if(!visited[neighbour]){
+                   return dfs(neighbour);
+               }
+
+           });
+        })(start);
+        return result;
+    }
+    depthFirstSearchIterative(start){
+        var stack = [start];
+        var result = [];
+        var visited = {};
+        visited[start] = true;
+        
+        var currenVertex;
+        while(stack.length){
+
+            currenVertex = stack.pop();
+            result.push(currenVertex);
+            this.ajacencyList[currenVertex].forEach(neighbour=>{
+               if(!visited[neighbour]){
+                   visited[neighbour] = true ;
+                   stack.push(neighbour);
+               }
+            });
+
+        }
+        return result;
+    }
+    breathFirstSearch(start){
+        var queue = [start];
+        var result = [];
+        var visited = {};
+        visited[start] = true;
+        var currenVertex;
+
+        while(queue.length){
+            currenVertex = queue.shift();
+            result.push(currenVertex);
+            this.ajacencyList[currenVertex].forEach(neighbour=>{
+               if(!visited[neighbour]){
+                   visited[neighbour]= true;
+                   queue.push(neighbour);
+               }
+            });
+        }
+        return result;
+    }
 }
 let g = new Graph();
-g.addVertex("Dallas");
-g.addVertex("Tokyo");
-g.addVertex("Aspen");
-g.addVertex("Los Angeles");
-g.addVertex("Hong Kong")
-g.addEdge("Dallas", "Tokyo");
-g.addEdge("Dallas", "Aspen");
-g.addEdge("Hong Kong", "Tokyo");
-g.addEdge("Hong Kong", "Dallas");
-g.addEdge("Los Angeles", "Hong Kong");
-g.addEdge("Los Angeles", "Aspen");
-g.removeVertex("Los Angeles");
+
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B","D")
+g.addEdge("C","E")
+g.addEdge("D","E")
+g.addEdge("D","F")
+g.addEdge("E","F")
 console.log(g);
+console.log(g.depthFirstSearchRecursive("A"));
+console.log(g.depthFirstSearchIterative("A"));
+console.log(g.breathFirstSearch("A"));
+
